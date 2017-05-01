0af1482a6941c85e4f5f662e1924518c7b080aac
Dennis Wu <dennisvwu@gmail.com>
2017-05-01 16:21:33 -0700

    changes
    attempting file transfer function

--- cli.py
+++ cli.py
@@ -5,8 +5,70 @@
 # Summary: this file implements a simplified FTP client application
 #=====================================================================
 import socket
+
+def Main():
+	host = '127.0.0.1'
+	port = 5000
+
+	# create TCP socket
+	s = socket.socket()
+	s.connect((host, port))
+
+	filename = raw_imput("Filename? -> ")
+
+	# check for quit
+	if filename != 'q':
+		s.send(filename)
+		data = s.recv(1024)
+
+		# check first 6 chars in data
+		if data[:6] == 'EXISTS':
+			filesize = long(data[6:])
+
+			message = raw_imput("File Exists, " + str(filesize) + "Bytes, download? Y/N? -> ")
+
+			if message == 'Y':
+				s.send('OK')
+				# open in write binary mode
+				f = open(filename, 'wb')
+				data = s.recv(1024)
+				totalRecv = len(data)
+				f.write(data)
+				# if more than 1024 bytes of data
+				while totalRecv < filesize:
+					data = s.recv(1024)
+					totalRecv += len(data)
+					# write data
+					f.write(data)
+					# two floating decimal point
+					print "{0:.2f}".format((totalRecv/float(filesize)) * 100) + "% Done"
+
+				print "Download Complete!"
+
+		else:
+			print "File does not Exists!"
+
+	# close socket
+	s.close()
+
+if __name__ == '__main__':
+	Main()
+
+
+
+
+
+
+
+
+
+
+
+
+
 import os
 import sys
+
 
 # Command line checks
 if len(sys.argv) < 2:
@@ -45,7 +107,6 @@ while True:
 	# Make sure we did not hit EOF
 	if fileData:
 
-
 		# Get the size of the data read
 		# and convert it to string
 		dataSizeStr = str(len(fileData))
@@ -54,7 +115,6 @@ while True:
 		# until the size is 10 bytes
 		while len(dataSizeStr) < 10:
 			dataSizeStr = "0" + dataSizeStr
-
 
 		# Prepend the size of the data to the
 		# file data.
--- serv.py
+++ serv.py
@@ -5,6 +5,64 @@
 # Summary: this file implements a simplified FTP server application
 #=====================================================================
 import socket
+import threading
+import os
+
+def GetFile(name, sock):
+
+    filename = sock.recv(1024)
+
+    if os.path.isfile(filename):
+
+            sock.send("EXISTS " + str(os.path.getsize(filename)))
+            userResponse = sock.recv(1024)
+
+            if userResponse[:2] == 'OK':
+                # read binary
+                with open(filename, 'rb') as f:
+                    bytesToSend = f.read(1024)
+                    sock.send(bytesToSend)
+
+                    # keep looping if file size larger than 1024 bytes
+                    while bytesToSend != "":
+                        bytesToSend = f.read(1024)
+                        sock.send(bytesToSend)
+
+    else:
+        # if file does not exist
+        sock.send("ERR")
+
+    # close connection
+    sock.close()
+
+def Main():
+
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket()
+    s.bind ((host, port))
+
+    # start listening
+    s.listening(5)
+
+    print "Server Started."
+	print "Waiting for connections..."
+
+    while True:
+        c, addr = s.accept()
+        print "Accepted connection from client:" + str(addr)
+        t = threading.Thread(target=GetFile, args=("retrThread", c))
+        t.start()
+
+
+    s.close()
+
+if __name__ == '__main__':
+    Main()
+
+
+
 
 # The port on which to listenPort
 listenPort = 1234
@@ -17,7 +75,6 @@ welcomeSock.bind(('', listenPort))
 
 # Start listening on the socket
 welcomeSock.listen(1)
-
 
 # ************************************************
 # Receives the specified number of bytes
